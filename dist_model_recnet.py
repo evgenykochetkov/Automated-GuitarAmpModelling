f357289ad5991b3a8b0e649ed43b55f28ec8dea1
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2026-02-14 22:06:27 +0300

    fix the “unexpected keyword argument” error

--- dist_model_recnet.py
+++ dist_model_recnet.py
@@ -85,7 +85,7 @@ def main(args):
 
     # Set up training optimiser + scheduler + loss fcns and training info tracker
     optimiser = torch.optim.Adam(network.parameters(), lr=args.learn_rate, weight_decay=1e-4)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', factor=0.5, patience=5, verbose=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', factor=0.5, patience=5)
     loss_functions = training.LossWrapper(args.loss_fcns, args.pre_filt)
     train_track = training.TrainTrack()
     writer = SummaryWriter(os.path.join('TensorboardData', model_name))
